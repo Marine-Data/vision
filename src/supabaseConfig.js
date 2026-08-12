ee378bd3cd3d52aa6db3fd70b0bda695549f9bfa
Marine-Data <m.zinzen04@gmail.com>
2026-08-12 15:52:43 +0200

    Create supabaseConfig.js

---
+++ src/supabaseConfig.js
@@ -0,0 +1,3 @@
+// Remplace par tes vraies valeurs (trouvables dans Supabase > Project Settings > API)
+export const SUPABASE_URL = 'https://osqycxwwbbwjwaubtvc.supabase.co';
+export const SUPABASE_ANON_KEY = 'TA_CLE_ANON_PUBLIC_ICI'; // Mets ta vraie clé ici
