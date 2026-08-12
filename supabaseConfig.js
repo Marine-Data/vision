1fe135c57fccbd4c4a7430d86f16b3584c5f3ea1
Marine-Data <m.zinzen04@gmail.com>
2026-08-12 15:56:58 +0200

    Update supabaseConfig.js

--- supabaseConfig.js
+++ supabaseConfig.js
@@ -7,5 +7,5 @@
 //   sont protégées par la sécurité RLS activée dans le SQL.)
 // ============================================================
 
-export const SUPABASE_URL = 'https://TON-PROJET.supabase.co';
-export const SUPABASE_ANON_KEY = 'TA_CLE_ANON_PUBLIQUE';
+export const SUPABASE_URL = 'https://osqyczwawbbwjwaubtvc.supabase.co/rest/v1/';
+export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9zcXljendhd2Jid2p3YXVidHZjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE5Nzk1MjUsImV4cCI6MjA5NzU1NTUyNX0.Lk2iuL3v_AGuXbyG5yU_-cfvnOtmYoWxiYdyCVKxXzg';
