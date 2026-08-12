db5d83be34c23b06b495b77fee979b20ba446f0f
Marine-Data <m.zinzen04@gmail.com>
2026-08-12 16:08:07 +0200

    Update supabaseConfig.js

--- src/supabaseConfig.js
+++ src/supabaseConfig.js
@@ -1,3 +1,2 @@
-// Remplace par tes vraies valeurs (trouvables dans Supabase > Project Settings > API)
 export const SUPABASE_URL = 'https://osqycxwwbbwjwaubtvc.supabase.co';
-export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9zcXljendhd2Jid2p3YXVidHZjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE5Nzk1MjUsImV4cCI6MjA5NzU1NTUyNX0.Lk2iuL3v_AGuXbyG5yU_-cfvnOtmYoWxiYdyCVKxXzg; // Mets ta vraie clé ici
+export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9zcXljendhd2Jid2p3YXVidHZjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE5Nzk1MjUsImV4cCI6MjA5NzU1NTUyNX0.Lk2iuL3v_AGuXbyG5yU_-cfvnOtmYoWxiYdyCVKxXzg';
